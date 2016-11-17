4c11a25e1f87e96cb798e41d75812866c424d5e9
Shelby Clayton and Sheena Nickerson <sheena@>
2016-11-17 11:23:48 -0800

    initial commit

---
+++ app/src/main/java/com/example/guest/localbusiness/LocationsActivity.java
@@ -0,0 +1,28 @@
+package com.example.guest.localbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class LocationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locations);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(LocationsActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/guest/localbusiness/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.guest.localbusiness;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.locationsButton) Button mLocationsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAboutButton.setOnClickListener(this);
+        mLocationsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAboutButton) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }
+        else if (v == mLocationsButton) {
+            Intent intent = new Intent(MainActivity.this, LocationsActivity.class);
+            startActivity(intent);
+        }
+    }
+}
